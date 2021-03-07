0b45a5c0e493524679137230a70eabccf2bbe195
ume388 <turi388ouji@gmail.com>
2021-03-07 01:51:52 +0000

    [add]aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get 'homes/about'
   resources :books, only: [:create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
