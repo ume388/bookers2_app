2b2284c27a3b1d5e2eb2b403385282587107c4a5
ume388 <turi388ouji@gmail.com>
2021-03-08 22:53:38 +0000

    [update]bookコントローラ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,53 @@
 class BooksController < ApplicationController
   
-  def new
-  end
-  
   def create
     @book = Book.new(book_params)
     @book_user_id = current_user.id
-    @book.save
-    redirect_to index
+    if @book.save
+      redirect_to book_path
+    else
+      @user = current_user
+      @books = Book.all
+      render 'index'
+    end
   end
   
   def index
+    @user = current_user
     @books = Book.all
     @book = Book.new
   end
   
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
   
   def edit
+    @book = book.find(params[:id])
+    if @book.user == current_user
+      render 'edit'
+    else
+      redirect_to books_path
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
   end
   
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+    end
   end
   
   private
