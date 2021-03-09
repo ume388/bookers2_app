012472fd41a7469e72e5c81fc06c4b901badc372
ume388 <turi388ouji@gmail.com>
2021-03-09 07:54:13 +0000

    [update]books'view

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aa4104b953b32103f89eda3d3539dae0d9758b5784d0d203085347ecd91db8615421125d9c9a79ce87bb8efa93e41eac7d31971e064eeb86ea14222e94cc4761'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about'
-  resources :books, only: [:create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :books
+  resources :users
 end
