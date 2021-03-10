9c88b495c6922107ba2139bc745534428e49430e
ume388 <turi388ouji@gmail.com>
2021-03-10 20:51:23 +0000

    [fix]users' 諸々

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(current_user)
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
