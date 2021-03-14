863aea8089b033f2338279299e733c1b8a9c72dd
ume388 <turi388ouji@gmail.com>
2021-03-14 09:41:14 +0000

    [change]名前とパスワードでログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about'
