a5dd069124c9d22d2beec7942e0a40b362bcc322
ume388 <turi388ouji@gmail.com>
2021-03-15 19:18:50 +0000

    [fix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   attachment :profile_image
   
   validates :name, presence: true, length: {minimum: 2, maximum: 20}
-  validates :introduction, presence: true, length: {maximum: 50}
+  validates :introduction, length: {maximum: 50}
   
 end
