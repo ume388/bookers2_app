637986afbf3952658ce14a43ff6c0a26663f557f
ume388 <turi388ouji@gmail.com>
2021-03-20 04:50:53 +0000

    [fix]home/about

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get '/home/about', to: 'homes#about'
+  get 'home/about', to: 'homes#about'
   resources :books
   resources :users, only: [:show, :index, :edit, :update]
 end
